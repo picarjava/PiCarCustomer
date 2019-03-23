8110b74c4c63a7f1defa41b533b9fe30b1baa6a7
Andy <abc623979@gmail.com>
2019-03-23 22:45:47 +0800

    bug fix add webSocket driver state

--- app/src/main/java/com/example/piCarCustomer/Constant.java
+++ app/src/main/java/com/example/piCarCustomer/Constant.java
@@ -2,6 +2,7 @@ package com.example.piCarCustomer;
 
 public interface Constant {
     String URL = "http://10.0.2.2:8081/PiCar";
+//    String URL = "http://192.168.137.1:8081/PiCar";
     String GOOGLE_DIRECTION_URL = "https://maps.googleapis.com/maps/api/directions/json?";
     String preference = "preference";
     String DRIVER_ID = "driverID";
