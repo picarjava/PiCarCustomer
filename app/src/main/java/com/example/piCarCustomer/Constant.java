b79eb1d11147bc3057aa79004e4ff0b4571304b3
Andy <abc623979@gmail.com>
2019-03-27 11:50:16 +0800

    add remote url

--- app/src/main/java/com/example/piCarCustomer/Constant.java
+++ app/src/main/java/com/example/piCarCustomer/Constant.java
@@ -1,11 +1,12 @@
 package com.example.piCarCustomer;
 
 public interface Constant {
-    String URL = "http://10.0.2.2:8081/PiCar";
-//    String URL = "http://192.168.137.1:8081/PiCar";
+//  String URL = "http://10.0.2.2:8081/PiCar";
+//  String WEB_SOCKET_URL = "ws://10.0.2.2:8081/PiCar";
     String GOOGLE_DIRECTION_URL = "https://maps.googleapis.com/maps/api/directions/json?";
-    String WEB_SOCKET_URL = "ws://10.0.2.2:8081/PiCar";
-//    String WEB_SOCKET_URL = "ws://192.168.137.1:8081/PiCar";
+
+    String URL = "http://35.221.225.234:8081/PiCar";
+    String WEB_SOCKET_URL = "ws://35.221.225.234:8081/PiCar";
     String preference = "preference";
     String DRIVER_ID = "driverID";
     String ORDER_ID = "orderID";
